ea088054e46af81da18cfa1b827d9b9497b93751
Dmitry Fomin <dmitry@everstay.net>
2025-11-09 16:59:19 -0800

    feat: create top-panel placeholder

---
+++ app/components/TopPanel/TopPanel.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export function TopPanel() {
+  return (
+    <header className='absolute top-0 left-0 right-0 z-50 h-16 grid grid-cols-3'>
+      <div></div>
+      <div>test</div>
+      <nav>aaa</nav>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { montserrat } from './fonts';
+import { TopPanel } from './components/TopPanel';
 
 export const metadata: Metadata = {
   title: 'The Birch House by Everstay',
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={` ${montserrat.className} antialiased`}>{children}</body>
+      <body className={` ${montserrat.className} antialiased`}>
+        <TopPanel />
+        {children}
+      </body>
     </html>
   );
 }
